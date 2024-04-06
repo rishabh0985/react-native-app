26e2227f7548ffe214ebaef9fd20a302c7b8574e
Rishabh kushwah <128125196+rishabh0985@users.noreply.github.com>
2024-04-06 11:59:58 +0530

    project01

---
+++ reactnativeFirstProject/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native'
+function App(){
+ return (
+  <SafeAreaView>
+  <view>
+    <text>
+      Hello world !
+    </text>
+  </view>
+</SafeAreaView>
+ )
+}
+
+export default App;
